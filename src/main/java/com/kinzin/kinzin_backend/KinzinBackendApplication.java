2180b91d946cebdbc29416242fc461cafc009e31
Myllena Moreira Miranda <myllenamiranda1411@gmail.com>
2024-07-21 16:32:22 -0300

    Initial commit

---
+++ src/main/java/com/kinzin/kinzin_backend/KinzinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kinzin.kinzin_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinzinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinzinBackendApplication.class, args);
+	}
+
+}
